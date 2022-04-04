2cb2f0269d18dfad2fd42d498f4d70395bf7c7fe
saurabh5shinde <98163539+saurabh5shinde@users.noreply.github.com>
2022-04-04 12:39:11 +0530

    Add files via upload

---
+++ Phase5/SelfAssisted/springboot-aws-1/src/main/java/com/saurabh/training/SpringbootAws1Application.java
@@ -0,0 +1,13 @@
+package com.saurabh.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAws1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAws1Application.class, args);
+	}
+
+}
